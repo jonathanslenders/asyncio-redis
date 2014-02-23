2205aca111059d6cc7bdec85cef9b43c9e78685c
Jonathan Slenders <jonathan@slenders.be>
2014-02-23 18:42:00 +0100

    Release 0.8 on pypi

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 setup(
         name='asyncio_redis',
         author='Jonathan Slenders',
-        version='0.7',
+        version='0.8',
         license='LICENSE.txt',
         url='https://github.com/jonathanslenders/asyncio-redis',
 

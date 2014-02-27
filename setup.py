880a416ba3a945570a60b85509cb7f53dd8f6046
Jonathan Slenders <jonathan@slenders.be>
2014-02-27 10:02:26 +0100

    Release 0.9 to Pypi. Now fully backwards compatible.
    - `get_next_published` was renamed to `next_published`
    - `get_as_list/set/dict` were renamed to `aslist/asset/asdict`
    - API change for LUA script execution, but support for complex return
      types instead.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ except ImportError:
 setup(
         name='asyncio_redis',
         author='Jonathan Slenders',
-        version='0.8',
+        version='0.9',
         license='LICENSE.txt',
         url='https://github.com/jonathanslenders/asyncio-redis',
 

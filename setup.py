3af2eeb42de6ea42d495e60c92598687b69d7283
Jonathan Slenders <jonathan@slenders.be>
2015-08-08 19:13:59 +0200

    Release 0.14.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 setup(
         name='asyncio_redis',
         author='Jonathan Slenders',
-        version='0.13.4',
+        version='0.14.1',
         license='LICENSE.txt',
         url='https://github.com/jonathanslenders/asyncio-redis',
 
